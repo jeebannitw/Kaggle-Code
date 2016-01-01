1a1768a19b270d152308f2475b24884b099f3a8b
jeebannitw <jeebannitw@gmail.com>
2016-01-01 15:55:29 +0530

    Update XGB_Code.R

--- XGB_Code.R
+++ XGB_Code.R
@@ -1,11 +1,18 @@
 ## Reference XGB Implementation R
 #https://www.kaggle.com/tqchen/otto-group-product-classification-challenge/understanding-xgboost-model-on-otto-data/notebook
 library(xgboost)
-setwd("C:\\Users\\jeeban\\Documents\\kaggle\\Give me Some Credit") # Set the working Directory
+setwd("C:/Users/User/Desktop/Kaggle Code/Give Me Some Credit") # Set the working Directory
 train<-read.csv("cs-training.csv") # Import train Data
 test<-read.csv("cs-test.csv") # Import Test data
 sample_submission<-read.csv("sampleEntry.csv")
+names(train)
+names(test)
+
 test$SeriousDlqin2yrs<-NULL ## To this Problem  Only ##(Drop Depedenet variable , as it is there is teseset)
+train$X<-NULL ## ID of the data to be dropped 
+test$X<-NULL ## ID of the data to be dropped
+
+
 ## Missing value treatment #
 sum(is.na(train)) ## Missing values train 
 sum(is.na(test)) # Missing values in test
@@ -13,8 +20,8 @@ sum(is.na(test)) # Missing values in test
 train[is.na(train)]<-999 ## replace missing values with 999
 test[is.na(test)]<-999 ## replace missing values with 999
 
-train[,2:11]<-log(train[,2:11]+1) ## Take log tranformations of allfeatures for better performance -train 
-test[,2:11]=log(test[,2:11]+1) ## Take log tranformations of allfeatures for better performance-test
+#train[,2:11]<-log(train[,2:11]+1) ## Take log tranformations of allfeatures for better performance -train 
+#test=log(test+1) ## Take log tranformations of allfeatures for better performance-test
 test_vars<-names(test) ## Test set variables 
 train_names<-names(train) ## train set variables ##
 common_vars<-intersect(test_vars,train_names) ## common variables set
@@ -49,20 +56,20 @@ clf <- xgb.cv(params              = param,
               maximize            = FALSE,
               eval_metric="auc",
               nfold=3
-              )
+)
 ####################
 ###### Use the entire training set using best parameters 
 clf_best <- xgboost(params        = param, 
                     data                = all_train, 
-                    nrounds             = 674, #300, #280, #125, #250, # changed from 300
+                    nrounds             = 420, #300, #280, #125, #250, # changed from 300
                     verbose             = 1,
                     #early.stop.round    = 200,
                     #watchlist           = watchlist,
                     maximize            = FALSE,
                     eval_metric="auc"
                     #nfold=3
-                    )
-                    
+)
+
 
 
 testDataMatrix<-xgb.DMatrix(data=data.matrix(test[,common_vars]))
@@ -71,6 +78,6 @@ pred = matrix(pred1, nrow=1)
 pred = data.frame(t(pred))
 names(pred)<-"Probability"
 pred$Id=sample_submission$Id
-write.csv(pred,file="XGB_Log_Transformation.csv",row.names=F)
+write.csv(pred,file="XGB_Log_Transformation1.csv",row.names=F)
 ###### Look at The Importance of Each variable ###
 importance_matrix <- xgb.importance(common_vars, model = clf_best)
